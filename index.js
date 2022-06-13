2bb1adc5e4ed019c971a3bea77822025b258e09a
ggclimdaw <guillpxtg@gmail.com>
2022-06-13 20:30:51 +0200

    Pràctica 3_v2

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const app = express()
 const port = 3000
@@ -8,5 +7,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log('Example app listening on port ${port}')
 })
